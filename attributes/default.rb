56f7ec6ae2c66c42fcbbb0d3d4abfbcdf03bca8b
Zen Kyprianou <zen@kyprianou.eu>
2015-04-18 01:39:21 +0100

    add default recipe

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default[:rbenv][:rubies] = ['2.2.2']
+default[:rbenv][:gems] = {
+  '2.2.2' => [
+    { 'name' => 'bundler' }
+  ]
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,13 @@
-#
-# Cookbook Name:: chef-rbenv_passenger
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+include_recipe "rbenv::ruby_build"
+include_recipe "rbenv::system"
+
+rbenv_gem 'passenger' do
+  action :upgrade
+end
+
+rbenv_script "passenger-install-nginx-module" do
+  rbenv_version node[:rbenv_passenger][:ruby_version]
+  code <<-BASH
+    passenger-install-nginx-module --auto
+  BASH
+end
