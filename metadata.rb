3eec6870525e1a2084d2145247c24c9e4320a11a
Zen Kyprianou <zen@kyprianou.eu>
2015-04-18 01:25:33 +0100

    add dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,10 @@ maintainer_email 'zen@kyprianou.eu'
 license          'MIT'
 description      'Installs rbenv and passenger on nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
+
+depends "rbenv"
+
+supports "ubuntu"
+supports "centos"
+supports "oracle"
