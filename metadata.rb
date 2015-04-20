19baa65145eec5d01735227b0a302006c1a8a3a7
Tom Bowden <tom.b1992@gmail.com>
2015-04-20 15:26:50 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'zen@kyprianou.eu'
 license          'MIT'
 description      'Installs rbenv and passenger on nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 depends "nginx"
 depends "rbenv"
